7a22bbce316d58da87d74675cbc06e7d33998448
calvo111979 <acalvoinfa10@gmail.com>
2021-03-22 15:59:16 +0100

    feat: para el número 1 devuelve 1

---
+++ test/numberToFizzBuzz.test.ts
@@ -0,0 +1,11 @@
+describe("Number to FizzBuzz", () => {
+  it("para el número 1 devuelve 1", () => {
+    const result = numberToFizzBuzz(1);
+
+    expect(result).toBe(1);
+  });
+});
+
+function numberToFizzBuzz(arg0: number) {
+  return 1;
+}
